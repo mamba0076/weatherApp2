88fb4fbbb9ae88491b32d542d4ea6dadeed24522
paul O <pmogolf@sky.com>
2021-02-01 14:04:56 +0000

    removed empty lines

--- public/js/app.js
+++ public/js/app.js
@@ -5,8 +5,6 @@ const messageOne = document.querySelector('#message-1')
 const messageTwo = document.querySelector('#message-2')
 
 
-
-
 weatherForm.addEventListener('submit', (e) => {
     e.preventDefault()
 
